c79de36e1733dad1ec8b6f02a2b5d84e81491636
AninHuang <lifeanne19904@gmail.com>
2019-10-03 23:53:52 +0800

    Create Channels Collection

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,61 +1,114 @@
-import React from 'react';
-import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import React from "react";
+import firebase from "../../firebase";
+import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
 
 class Channels extends React.Component {
   state = {
+    user: this.props.currentUser,
     channels: [],
-    channelName: '',
-    channelDetails: '',
+    channelName: "",
+    channelDetails: "",
+    channelsRef: firebase.database().ref("channels"),
     modal: false
-  }
+  };
+
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails, user } = this.state;
+
+    const key = channelsRef.push().key;
+
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL
+      }
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: "", channelDetails: "" });
+        this.closeModal();
+        console.log("channel added");
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.addChannel();
+    }
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
 
   openModal = () => this.setState({ modal: true });
 
   closeModal = () => this.setState({ modal: false });
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  render () {
+  render() {
     const { channels, modal } = this.state;
 
     return (
       <React.Fragment>
-        <Menu.Menu style={{ paddingBottom: '2em' }}>
+        <Menu.Menu style={{ paddingBottom: "2em" }}>
           <Menu.Item>
             <span>
               <Icon name="exchange" /> CHANNELS
-            </span>
-            ({ channels.length }) <Icon name="add" onClick={this.openModal}/>
+            </span>{" "}
+            ({channels.length}) <Icon name="add" onClick={this.openModal} />
           </Menu.Item>
+          {/* Channels */}
         </Menu.Menu>
 
+        {/* Add Channel Modal */}
         <Modal basic open={modal} onClose={this.closeModal}>
-          <Modal.Header>Add a channel</Modal.Header>
+          <Modal.Header>Add a Channel</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
-                <Input fluid label="Name of channel" name="channelName" onChange={this.handleChange}/>
+                <Input
+                  fluid
+                  label="Name of Channel"
+                  name="channelName"
+                  onChange={this.handleChange}
+                />
               </Form.Field>
 
               <Form.Field>
-                <Input fluid label="About the channel" name="channelDetails" onChange={this.handleChange}/>
+                <Input
+                  fluid
+                  label="About the Channel"
+                  name="channelDetails"
+                  onChange={this.handleChange}
+                />
               </Form.Field>
             </Form>
           </Modal.Content>
 
           <Modal.Actions>
-            <Button color="green" inverted>
-              <Icon name="checkmark"/> Add
+            <Button color="green" inverted onClick={this.handleSubmit}>
+              <Icon name="checkmark" /> Add
             </Button>
             <Button color="red" inverted onClick={this.closeModal}>
-              <Icon name="remove"/> Cancel
+              <Icon name="remove" /> Cancel
             </Button>
           </Modal.Actions>
         </Modal>
       </React.Fragment>
-    )
+    );
   }
 }
 
