3fb659e226562aa0def38dcbf502b8fbca0dd531
ada <d40228112@gmail.com>
2019-10-06 16:51:06 +0800

    7.8

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,19 @@ import { connect } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import './App.css';
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns="equal" className="app" style={{ background: '#eee' }}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser} />
+    <SidePanel
+      key={currentUser && currentUser.uid}
+      currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        cuurentUser={currentUser}
+      />
     </Grid.Column>
 
     <Grid.Column width={4}>
@@ -23,7 +29,8 @@ const App = ({ currentUser }) => (
 )
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
+});
 
 export default connect(mapStateToProps)(App);
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
+import Channels from './Channels'
 
 class SidePanel extends React.Component {
   render () {
